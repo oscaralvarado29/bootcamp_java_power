54d904da53b76fa911480e31c3bd51447796dbf8
oscaralvarado29 <oscar.alvarado.mt@correo.usa.edu.co>
2022-09-29 07:36:15 -0500

    agregar atributos de inicio, destino y paradas
    se agregan los atributos de inicio, destino y paradas con sus respectvos getters y setters

--- Semana2/creacion_de_rutas/src/main/java/com/rutas/conductor/creacion_de_rutas/domain/model/Route.java
+++ Semana2/creacion_de_rutas/src/main/java/com/rutas/conductor/creacion_de_rutas/domain/model/Route.java
@@ -1,16 +1,48 @@
 package com.rutas.conductor.creacion_de_rutas.domain.model;
 
+import java.util.List;
+
 public class Route {
     private Long routeId;
     private String description;
+    private String origin;
+    private String destination;
+    private List<String> stops;
     private Long quota;
     private Long conductorId;
 
-    public Route(Long routeId, String description, Long quota, Long conductorId) {
+    public Route(Long routeId, String description, String origin, String destination, List<String> stops, Long quota, Long conductorId) {
         this.routeId = routeId;
         this.description = description;
+        this.origin = origin;
+        this.destination = destination;
+        this.stops = stops;
         this.quota = quota;
         this.conductorId = conductorId;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public List<String> getStops() {
+        return stops;
+    }
+
+    public void setStops(List<String> stops) {
+        this.stops = stops;
     }
 
     public Long getRouteId() {
