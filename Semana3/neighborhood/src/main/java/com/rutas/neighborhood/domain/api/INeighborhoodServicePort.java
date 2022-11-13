91d044dd49485439206d187598d7834ce66e81f2
oscaralvarado29 <oscar.alvarado.mt@correo.usa.edu.co>
2022-11-12 20:59:44 -0500

    desarrollo proyecto neighborhood

--- Semana3/neighborhood/src/main/java/com/rutas/neighborhood/domain/api/INeighborhoodServicePort.java
+++ Semana3/neighborhood/src/main/java/com/rutas/neighborhood/domain/api/INeighborhoodServicePort.java
@@ -1,10 +1,10 @@
-package com.rutas.neighborhood.domain.spi;
+package com.rutas.neighborhood.domain.api;
 
 import com.rutas.neighborhood.domain.model.Neighborhood;
 
 import java.util.List;
 
-public interface INeighborhoodPersistencePort {
+public interface INeighborhoodServicePort {
     void saveNeighborhood(Neighborhood neighborhood);
     void updateNeighborhood(Neighborhood neighborhood);
     void deleteNeighborhood(String neighborhoodName);
